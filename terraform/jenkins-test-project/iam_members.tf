ed32a3fcfb470d5fc6dfe5903824203551345ec0
Andy Cockerill <cockerill.andrew@gmail.com>
2020-08-02 10:27:08 +0100

    project id

--- terraform/jenkins-test-project/iam_members.tf
+++ terraform/jenkins-test-project/iam_members.tf
@@ -3,7 +3,7 @@
 module "test-project-viewer" {
   source          = "../modules/user-iam-member"
   name            = var.test-project-viewer
-  project_id      = module.project.project_id
+  project_id      = var.project_id
   iam_binding     = var.project_viewer
   member          = var.test
 }
--- terraform/jenkins-test-project/service_accounts.tf
+++ terraform/jenkins-test-project/service_accounts.tf
@@ -3,7 +3,7 @@
 module "test-sa" {
   source          = "../modules/service-account"
   name            = var.test-sa
-  project_id      = module.project.project_id
+  project_id      = var.project_id
   sa_account_id   = var.test-sa
   sa_display_name = var.sa_display_name_test_sa
 }
