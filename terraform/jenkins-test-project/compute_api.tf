0edbadd793f33132c3937b0bce8cbb7bda087c9d
Andy Cockerill <cockerill.andrew@gmail.com>
2020-08-02 10:31:35 +0100

    var

--- terraform/jenkins-test-project/compute_api.tf
+++ terraform/jenkins-test-project/compute_api.tf
@@ -1,7 +1,7 @@
 module "compute_api" {
   source = "../modules/services"
   name                       = var.compute_name
-  project_id                 = module.project.project_id
+  project_id                 = var.project_id
   api_name                   = var.compute_api_name
   disable_dependent_services = var.compute_disable_dependent_services
     }
