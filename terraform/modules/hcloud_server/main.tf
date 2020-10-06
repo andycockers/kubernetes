c427612fd2cfdfddd53cc62a7ca00e963ef5fc09
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-10-06 11:39:40 +0100

    eek

--- terraform/modules/hcloud_server/main.tf
+++ terraform/modules/hcloud_server/main.tf
@@ -4,7 +4,7 @@ data "hcloud_image" "ubuntu_slave" {
 
 resource "hcloud_server" "node1" {
   name        = var.server_name
-  image       = data.hcloud_image.ubuntu_slave.id
+  image       = var.image_name
   server_type = var.server_type
   location    = var.location
 }
