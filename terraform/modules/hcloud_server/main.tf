d8290791f01e2cd5e352b309126564d01431007c
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-10-06 11:16:33 +0100

    snapshot shizzle

--- terraform/modules/hcloud_server/main.tf
+++ terraform/modules/hcloud_server/main.tf
@@ -1,6 +1,10 @@
+data "hcloud_image" "ubuntu_slave" {
+  with_selector = "type=ubuntu_slave"
+}
+
 resource "hcloud_server" "node1" {
   name        = var.server_name
-  image       = var.image_name
+  image       = data.hcloud_image.ubuntu_slave.id
   server_type = var.server_type
   location    = var.location
 }
