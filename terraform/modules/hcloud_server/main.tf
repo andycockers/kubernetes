5f3b1839508a466d503b5ca44aa3ce45163262fe
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-10-07 11:32:13 +0100

    arf

--- terraform/modules/hcloud_server/main.tf
+++ terraform/modules/hcloud_server/main.tf
@@ -4,7 +4,7 @@ data "hcloud_image" "ubuntu_slave" {
 
 resource "hcloud_server" "node1" {
   name        = var.server_name
-  image       = var.image_name #The ID is displayed when the image is created
+  image       = data.hcloud_image.ubuntu_slave.id #The ID is displayed when the image is created
   server_type = var.server_type
   location    = var.location
 
