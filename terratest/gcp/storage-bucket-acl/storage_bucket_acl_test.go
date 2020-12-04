feae033f92b46a85911685ae4a5eee1697b37f46
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-04 17:02:00 +0000

    terratest storge bucket acl

---
+++ terratest/gcp/storage-bucket-acl/storage_bucket_acl_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestStorageBucket(t *testing.T) {
+	t.Parallel()
+
+	bucketName := fmt.Sprintf("terratest-%s", strings.ToLower(random.UniqueId()))
+
+	bucketOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../../terraform/modules/storage-bucket",
+
+		Vars: map[string]interface{}{
+			"project_id":  "jenkins-test-project-284609",
+			"bucket_name": bucketName,
+			"location":    "EU",
+			"name":        bucketName,
+		},
+		
+	}
+)
+
+	defer terraform.Destroy(t, bucketOptions)
+	terraform.InitAndApply(t, bucketOptions)
+
+	bucket_name := terraform.Output(t, bucketOptions, "bucket_name")
+
+	aclOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../../terraform/modules/storage-bucket-acl",
+
+		Vars: map[string]interface{}{
+			"project_id":  "jenkins-test-project-284609",
+			"bucket_name": bucket_name,
+			"name":        bucket_name,
+		},
+		
+	}
+)
+
+
+}
