afb0630811b641021c2317ae5ac96531c0d2ea90
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-04 18:06:06 +0000

    omg

--- terraform/modules/google_compute_instance/main.tf
+++ terraform/modules/google_compute_instance/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "default" {
   name         = var.gce_instance_name
   machine_type = var.machine_type
   zone         = var.zone
-  project      = vap.project_id
+  project      = var.project_id
 
     boot_disk {
     initialize_params {
