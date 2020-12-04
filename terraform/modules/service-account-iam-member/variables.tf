f3d9e58a8de2c539a3d5ff64142cb130327f03c5
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-04 14:57:03 +0000

    I still dont know

--- terraform/modules/service-account-iam-member/variables.tf
+++ terraform/modules/service-account-iam-member/variables.tf
@@ -1,4 +1,4 @@
-#variable "name" {}
+variable "name" {}
 variable "project_id" {}
 variable "role" {}
 variable "member" {}
--- terratest/gcp/service-account-iam-member/service_account_iam_member_test.go
+++ terratest/gcp/service-account-iam-member/service_account_iam_member_test.go
@@ -15,7 +15,7 @@ func TestServiceAccountIAMMember(t *testing.T) {
 			"project_id":      "jenkins-test-project-284609",
 			"sa_account_id":   "terratest",
 			"sa_display_name": "Terratest role",
-			"name":            "Terratest",
+			"name":            "",
 		},
 	},
 	)
@@ -32,7 +32,7 @@ func TestServiceAccountIAMMember(t *testing.T) {
 			"project_id": "jenkins-test-project-284609",
 			"role":       "roles/storage.objectViewer",
 			"member":     email,
-			//"name":       "Terratest",
+			"name":       "",
 		},
 	},
 	)
