c6dabec68a9194d907c5bf6c3d9b97a55c2b8bad
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-08 15:48:04 +0000

    maybe this time

--- terratest/gcp/service-account-iam-member/service_account_iam_member_test.go
+++ terratest/gcp/service-account-iam-member/service_account_iam_member_test.go
@@ -1,19 +1,31 @@
 package test
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
+func RandomString(n int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyz")
+	s := make([]rune, n)
+	for i := range s {
+		s[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(s)
+}
 func TestServiceAccountIAMMember(t *testing.T) {
 	//t.Parallel()
+
+	saID := fmt.Sprintf(RandomString(10))
 
 	optionsServiceAccount := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		TerraformDir: "../../../terraform/modules/service-account",
 		Vars: map[string]interface{}{
 			"project_id":      "jenkins-test-project-284609",
-			"sa_account_id":   "serviceaccountiam",
+			"sa_account_id":   saID,
 			"sa_display_name": "Terratest role",
 			"name":            "",
 		},
