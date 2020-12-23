96953ade5ca660d5cee0da51f9bdfbec81fd4e70
Andrew Cockerill <andrew.cockerill@appnovation.com>
2020-12-23 11:31:25 +0000

    really?

--- terraform/hetzner-server/provider.tf
+++ terraform/hetzner-server/provider.tf
@@ -1,3 +1,14 @@
-provider "hcloud" {
-  token = var.hcloud_token
+terraform {
+  required_providers {
+    hcloud = {
+      # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+      # provider plugins.
+      #
+      # source is not required for the hashicorp/* namespace as a measure of
+      # backward compatibility for commonly-used providers, but recommended for
+      # explicitness.
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.23.0"
+    }
+  }
 }
