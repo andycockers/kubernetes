d93f1d46e6f63b695c0edfce57bdc619ea5c7681
Andrew Cockerill <andrew.cockerill@appnovation.com>
2021-01-05 09:07:48 +0000

    added string type

--- terraform/modules/hcloud_server/variables.tf
+++ terraform/modules/hcloud_server/variables.tf
@@ -3,4 +3,4 @@ variable "server_name"  {}
 variable "image_name"   {}
 variable "server_type"  {}
 variable "location"     {}
-variable "ssh_keys"     {}
+variable "ssh_keys"     {type = list(string)}
