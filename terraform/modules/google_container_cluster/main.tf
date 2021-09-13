8cf3abd13780eeae5ab193d02018f7468566ce33
cockers <cockerill.andrew@gmail.com>
2021-09-13 14:44:14 +0100

    erm

--- terraform/modules/google_container_cluster/main.tf
+++ terraform/modules/google_container_cluster/main.tf
@@ -35,9 +35,6 @@ resource "google_container_cluster" "primary" {
   identity_namespace = "${var.project_id}.svc.id.goog"
 }
 
-  workload_metadata_config {
-    node_metadata = "GKE_METADATA_SERVER"
-  }
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
@@ -51,6 +48,10 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     preemptible  = true
     machine_type = var.machine_type
     disk_size_gb = var.disk_size_gb
+
+    workload_metadata_config {
+    node_metadata = "GKE_METADATA_SERVER"
+  }
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = "jenkins-test-project-284609@appspot.gserviceaccount.com"
