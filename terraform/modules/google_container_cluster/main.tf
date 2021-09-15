b8949402bb0745846d9e251ac893a580b2c5b8a9
cockers <cockerill.andrew@gmail.com>
2021-09-15 11:24:05 +0100

    changes

--- terraform/modules/google_container_cluster/main.tf
+++ terraform/modules/google_container_cluster/main.tf
@@ -1,9 +1,3 @@
-resource "google_service_account" "default" {
-  account_id   = "kubernetes-service-account"
-  display_name = "Service Account"
-  project      = var.project_id
-}
-
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.cluster_location
@@ -31,10 +25,6 @@ resource "google_container_cluster" "primary" {
     enabled = false
   }
 
-  workload_identity_config {
-  identity_namespace = "${var.project_id}.svc.id.goog"
-}
-
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
@@ -48,15 +38,6 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     preemptible  = true
     machine_type = var.machine_type
     disk_size_gb = var.disk_size_gb
-
-    workload_metadata_config {
-    node_metadata = "GKE_METADATA_SERVER"
-  }
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = "andyc-963@jenkins-test-project-284609.iam.gserviceaccount.com"
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    
   }
 }
